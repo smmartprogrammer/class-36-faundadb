79fb5f2ca5f0d76646ef75ea03abc612b40959f4
Raghunathji <partabrajp@gmail.com>
2021-05-24 23:50:42 +0500

    update faunda db

---
+++ step1.js
@@ -0,0 +1,17 @@
+const faunadb = require('faunadb');
+const q = faunadb.query;
+
+
+(async()=>{
+    const client = faunadb.Client({secret:"fnAEKAU8RMACBcTiy4myzse2_07nN__45Sfye8Kg"
+
+    try {
+        const result = client.query(
+
+        )
+    }
+    catch(error) {
+
+    }
+    })
+})
