7155c5892523112964dfb566d8fbf6b565a6193e
Raghunathji <partabrajp@gmail.com>
2021-05-24 23:57:31 +0500

    update git

--- step1.js
+++ step1.js
@@ -3,15 +3,24 @@ const q = faunadb.query;
 
 
 (async()=>{
-    const client = faunadb.Client({secret:"fnAEKAU8RMACBcTiy4myzse2_07nN__45Sfye8Kg"
+    const client = faunadb.Client({secret:"fnAEKAU8RMACBcTiy4myzse2_07nN__45Sfye8Kg"})
 
     try {
         const result = client.query(
+            q.CreateDatabase("gatsbyapp")
 
         )
+        console.log("Result =", result)
     }
     catch(error) {
+        if (error.requestResult.statusCode === 400 && error.message === 'instance already exists') {
+            console.log('Database with this name already exists');
+          }
+          else {
+            console.log('Unknow Error: ');
+            console.log(error);
+          }
 
-    }
+    
     })
 })
