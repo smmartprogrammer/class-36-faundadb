68783ed185ee8698bf61dced5d6c4c4aea5e17fc
Raghunathji <partabrajp@gmail.com>
2021-05-25 00:17:49 +0500

    update git

--- step1.js
+++ step1.js
@@ -2,22 +2,30 @@ const faunadb = require('faunadb');
 const q = faunadb.query;
 
 (async () => {
-  const client = faunadb.Client({
+  const client = new faunadb.Client({
     secret: 'fnAEKAU8RMACBcTiy4myzse2_07nN__45Sfye8Kg',
   });
 
   try {
-    const result = client.query(q.CreateDatabase('gatsbyapp'));
+    // const result = await client.query(q.CreateDatabase({ name: 'gatsbyapp' }));
+    // console.log('Result =', result);
+    const result = await client.query(
+      q.CreateKey({
+        database: q.Database('mytestdatabase'),
+        role: 'server',
+      }),
+    );
     console.log('Result =', result);
   } catch (error) {
-    if (
-      error.requestResult.statusCode === 400 &&
-      error.message === 'instance already exists'
-    ) {
-      console.log('Database with this name already exists');
-    } else {
-      console.log('Unknow Error: ');
-      console.log(error);
-    }
+    console.log('error = ', result);
+    // if (
+    //   error.requestResult.statusCode === 400 &&
+    //   error.message === 'instance already exists'
+    // ) {
+    //   console.log('Database with this name already exists');
+    // } else {
+    //   console.log('Unknow Error: ');
+    //   console.log(error);
+    // }
   }
 })();
